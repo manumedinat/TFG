f03416fccbbdd230681be125e31df623cce1a4cb
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-01 13:35:37 +0100

    Create Heroes.java

---
+++ prueba/src/main/java/com/example/demo/dao/entity/Heroes.java
@@ -0,0 +1,24 @@
+package com.example.demo.dao.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+
+import java.io.Serializable;
+
+@Data
+@EqualsAndHashCode
+@Entity
+public class Heroes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID_Heroes", nullable = false)
+    private String episode;
+
+    @Column(name = "char_id", nullable = false)
+    private String charid;
+}
