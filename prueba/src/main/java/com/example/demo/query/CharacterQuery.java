607e565edf99240239ecfa58ee33cd615b72ac0a
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-01 13:36:14 +0100

    Create CharacterQuery.java

---
+++ prueba/src/main/java/com/example/demo/query/CharacterQuery.java
@@ -0,0 +1,18 @@
+package com.example.demo.query;
+
+import java.util.Optional;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.example.demo.dao.entity.Character;
+import com.example.demo.service.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CharacterQuery implements GraphQLQueryResolver {
+    @Autowired
+    private CharacterService characterService;
+    public Iterable<Character> listCharacter(String id, String fname, String lname, String type) {
+        return this.characterService.getAllCharacters(id,fname,lname,type);
+    }
+}
