f9009d72a9e0b0c1b4ff5cd75660d9f33be7168f
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-02 20:31:22 +0100

    Delete VehicleRepository.java

--- prueba/src/main/java/com/example/demo/dao/repository/VehicleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao.repository;
-import com.example.demo.dao.entity.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
-}
