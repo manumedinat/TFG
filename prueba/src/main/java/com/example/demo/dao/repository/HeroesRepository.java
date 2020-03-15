d956695eae657e0e309dbacfa01a61559ad6c289
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-15 18:38:55 +0100

    Update HeroesRepository.java

--- prueba/src/main/java/com/example/demo/dao/repository/HeroesRepository.java
+++ prueba/src/main/java/com/example/demo/dao/repository/HeroesRepository.java
@@ -7,5 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface HeroesRepository extends JpaRepository<Heroes, String> {
-List<Heroes> findHeroesByCharidOrEpisode(String episode, String charid); 
+List<Heroes> findHeroesByEpisodeidOrCharid(String episodeid, String charid); 
+List<Heroes> findByEpisodeidOrCharid(String episodeid, String charid); 
+List<Heroes> findAllByEpisodeid(String episodeid); 
+List<Heroes> findAllByCharid(String charid); 
+//List<Heroes> findHeroesByEpisodeid(String episodeid);
+
 }
