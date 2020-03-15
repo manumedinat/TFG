c2f7768f65af615dfb1bab88c70dbb15e0ddd3e0
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-15 18:40:30 +0100

    Fallo al meter filtro
    Hace bien la query pero al meter filtro devuelve lo mismo.

--- prueba/src/main/java/com/example/demo/resolver/AppearsResolver.java
+++ prueba/src/main/java/com/example/demo/resolver/AppearsResolver.java
@@ -1,36 +1,37 @@
 package com.example.demo.resolver;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
 import com.coxautodev.graphql.tools.GraphQLResolver;
-import com.example.demo.dao.*;
 import com.example.demo.dao.entity.Appears;
 import com.example.demo.dao.entity.Episode;
+import com.example.demo.dao.repository.AppearsRepository;
 import com.example.demo.dao.repository.EpisodeRepository;
 
 import org.springframework.stereotype.Component;
 
-import com.example.demo.dao.repository.AppearsRepository;
+
 
 @Component
 public class AppearsResolver implements GraphQLResolver<Appears> {
     private final EpisodeRepository episodeRepository;
-    // private AppearsRepository appearsRepository;
+    private final AppearsRepository appearsRepository;
+ 
 
-    public AppearsResolver(final EpisodeRepository episodeRepository) {
+    public AppearsResolver(final EpisodeRepository episodeRepository, final AppearsRepository appearsRepository) {
         this.episodeRepository = episodeRepository;
+        this.appearsRepository= appearsRepository;
     }
 
-    public List<Episode> getEpisode(Appears appears, final String eid, final String ecode) {
-       // return episodeRepository.findEpisodeByeidOrEcode(eid, ecode);
-            if(appears.getEpisodeid() != null) {
+    public Iterable<Episode> getEpisode(Appears appears,  String eid,  String ecode) {
+        if(appearsRepository.findByCharid(appears.getCharid()).isEmpty()){
+            return episodeRepository.findAll();
+        }
+        return episodeRepository.findEpisodeByEidOrEcode(eid, ecode);
+           /* if(episodeRepository.findEpisodeByeidOrEcode(eid, ecode).isEmpty()) {
             return episodeRepository.findAll().stream().filter((episode-> appears.getEpisodeid().equals(eid))).collect(Collectors.toList());
             }else{
                 return episodeRepository.findAll();
-            }
+            }*/
         }
 
 }
