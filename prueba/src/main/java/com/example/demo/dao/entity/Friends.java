fd356b33366ec7a0fe85a23e896ffec39d4ba0cc
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-15 18:37:58 +0100

    Update Friends.java

--- prueba/src/main/java/com/example/demo/dao/entity/Friends.java
+++ prueba/src/main/java/com/example/demo/dao/entity/Friends.java
@@ -2,24 +2,25 @@ package com.example.demo.dao.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
 import javax.persistence.*;
 
+import org.checkerframework.common.aliasing.qual.Unique;
 
 import java.io.Serializable;
 
 @Data
 @EqualsAndHashCode
 @Entity
+@Table(name= "friends")
 public class Friends implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
+    @Column(name= "ID",unique = false)
    // @Column(name = "ID_Friends", nullable = false)
     private String id;
-    
-    
-   // @Column(name = "fid", nullable = false)
+
+    @Column(name = "FR_ID")
     private String fid;
 }
