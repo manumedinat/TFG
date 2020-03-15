2c8879452989b077739f0379ad7320be643f9d77
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-15 18:38:10 +0100

    Update AppearsRepository.java

--- prueba/src/main/java/com/example/demo/dao/repository/AppearsRepository.java
+++ prueba/src/main/java/com/example/demo/dao/repository/AppearsRepository.java
@@ -1,11 +1,18 @@
 package com.example.demo.dao.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.dao.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface AppearsRepository extends JpaRepository<Appears, String> {
 List <Appears> findByCharid(String charid);
+
+//@Query(" where (charid = coalesce(?:charid,charid))")
+List <Appears> findAppearsInByCharid(String charid);
+//List <Episode> findByEpisodes(List<Episode> episodes);
 }
