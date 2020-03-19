25b8248af437304d341e91128bda8102c44503bb
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-03-19 18:47:25 +0100

    Nuevas funciones
    - Datos de join
    - Obtener datatype y attribute

--- R2RML/funciones_ttl.js
+++ R2RML/funciones_ttl.js
@@ -1,10 +1,11 @@
+//import { json } from 'express';
 var fs= require('fs');
 var _= require('lodash');
 var file= fs.readFileSync("./mappings.r2rml.json"); //leer fichero en formato utf8
 var jsonFile= JSON.parse(file); //parsear fichero 
 
 /*1. Funcion para obtener el TriplesMap*/
-function getTriplesId(){
+exports.getTriplesId= function(){
 var array=[];
 var elemento;
     for (var j in jsonFile["@graph"]){
@@ -20,20 +21,19 @@ var elemento;
     return array;
     
 }
-const result=getTriplesId();
-console.log('Tarea 1: Obtener todos los Triples Maps:')
+/*const result= this.getTriplesId();
 console.log(result);
 
 /********************************************************************* */
 /*2. Función para obtener subjectMap, predicateObjectMap y logicalTable a partir del mapa de tripletas dado */
 
-function getIdsFromTripleMap(TriplesId){
+exports.getIdsFromTripleMap= function(triplesId){
     var elemento2, logicalTableId, subjectMapId;
     var predicateObjectMapIds=[];
     for (var j in jsonFile["@graph"]){
         elemento2= jsonFile["@graph"][j];
 
-        if(elemento2["@id"]===TriplesId){
+        if(elemento2["@id"]===triplesId){
                 logicalTableId=elemento2["rr:logicalTable"]["@id"]; 
                 subjectMapId=elemento2["rr:subjectMap"]["@id"];
                 if(elemento2['rr:predicateObjectMap'].length==null){
@@ -48,18 +48,19 @@ function getIdsFromTripleMap(TriplesId){
         }
     return {logicalTableId, predicateObjectMapIds, subjectMapId};
 }
-console.log(`\nTarea 2: Obtener Subject, PO Map y Logical Table a partir de tripleta`)
-const result1= getIdsFromTripleMap(result[3]);
+
+/*console.log(`\nTarea 2: Obtener Subject, PO Map y Logical Table a partir de tripleta`)
+const result1= this.getIdsFromTripleMap(result[4]);
 const logicalTableId= result1.logicalTableId;
 const predicateObjectMapIds= result1.predicateObjectMapIds;
 const subjectMapId=result1.subjectMapId;
-console.log(`Datos de TRIPLES MAP: ${result[3]}\n`);
+console.log(`Datos de TRIPLES MAP: ${result[4]}\n`);
 console.log(`Logical Table = ${logicalTableId}`);
 console.log(`Predicate Object Maps = ${predicateObjectMapIds}`);
 console.log(`Subject Map = ${subjectMapId}`);
 /****************************************************************************** */
 /*3. Función para obtener Predicate y ObjectMap a partir del PredicateObjectMap*/
-function getIdsFromPredObjMap(PredObjId){
+exports.getIdsFromPredObjMap= function(PredObjId){
     var objectMapId, predicateId,elemento3;
     for (var j in jsonFile["@graph"]){
         elemento3= jsonFile["@graph"][j];
@@ -71,47 +72,86 @@ function getIdsFromPredObjMap(PredObjId){
     }
     return {objectMapId, predicateId};
 }
-console.log(`\nTarea 3: Obtener predicate y object Map`)
-const result2= getIdsFromPredObjMap(predicateObjectMapIds[1]);
+
+/*console.log(`\nTarea 3: Obtener predicate y object Map`)
+const result2= this.getIdsFromPredObjMap(predicateObjectMapIds[0]);
 const predicateId= result2.predicateId;
 const objectMapId= result2.objectMapId;
 console.log(`Object Map = ${objectMapId}`);
 console.log(`Predicate = ${predicateId}`);
 /***************************************** */
 
-/*4. Función para obtener nombre tabla a partir de logicalTable*/
-function getTableName (logTableId){
-    var tableName, elemento4;
-    for (var j in jsonFile["@graph"]){
-        elemento4= jsonFile["@graph"][j];
+/*4. Función para obtener nombre de la clase a partir de un subject map*/
 
-        if(elemento4["@id"]===logTableId){
-            tableName= elemento4['rr:tableName'];
+exports.getClassNameFromSubjMap=function(subjMapId){
+    var className, aux,elemento4;
+    for(var j in jsonFile["@graph"]){
+        elemento4 = jsonFile["@graph"][j];
+
+        if(elemento4['@id']=== subjMapId){
+            aux= elemento4['rr:class']['@id'];
+            className= aux.split(":")[1];
         }
     }
-    return tableName;
+    return className;
 }
-console.log(`\nTarea 4: Obtener nombre de tabla`);
-const result3= getTableName(logicalTableId);
-console.log(`Table Name = ${result3}`);
-/*************************************************** */
-/*5. Obtener joinCondition y ParentTriplesMap si existe; Sino, obtener template */ 
-function getIdsFromObjectMap (ObjMapId){
-    var joinConditionId, parentTriplesMapId, elemento4;
-    for (var j in jsonFile["@graph"]){
-        elemento5= jsonFile["@graph"][j];
+/*
+console.log(`\nTarea 4: Obtener Class Name`)
+const result3= this.getClassNameFromSubjMap(subjectMapId);
+console.log(`Class Name = ${result3}`);
+*/
 
-        if((elemento5["@id"]===ObjMapId) && elemento5.hasOwnProperty('rr:joinCondition')){
-            joinConditionId= elemento5['rr:joinCondition']['@id'];
-            parentTriplesMapId= elemento5['rr:parentTriplesMap']['@id'];
+/*5. Función para obtener datatype a partir de un objectMap */
+exports.getDataTypeFromObjMap=function(objMapId){
+    var dataType, aux2,elemento4;
+    for(var j in jsonFile["@graph"]){
+        elemento4 = jsonFile["@graph"][j];
+
+        if(elemento4['@id']=== objMapId){
+            if(!elemento4['rr:joinCondition']){
+            dataType= elemento4['rr:column'];
+            }else if (!elemento4['rr:column']){
+            aux2=elemento4['rr:joinCondition']['@id'];    
+            dataType= this.getIdsFromJoin(aux2).child;
+           }else if(!elemento4['rr:joinCondition'] && !elemento4['rr:column']) {
+             dataType= elemento4 ['rr:template'];
         }
     }
-    return {joinConditionId, parentTriplesMapId};
 }
-console.log(`\nTarea 5: Obtener join y tripleta padre`);
-const result4= getIdsFromObjectMap(objectMapId);
-const joinConditionId= result4.joinConditionId;
-const parentTriplesMapId= result4.parentTriplesMapId;
-console.log(`Join Condition = ${joinConditionId}`);
-console.log(`Parent Triples Map = ${parentTriplesMapId}`);
-/********************************************************* */
+    return dataType;
+}
+
+/*console.log(`\nTarea 5: Obtener Data Type`)
+const result4= this.getDataTypeFromObjMap(objectMapId);
+console.log(`Data Type = ${result4}`);
+
+
+/*6. Función para obtener atributo a partir de un predicateMap */
+exports.getAttributeFromPredMap=function(predMapId){
+        var attribute= predMapId.split(":")[1];
+        return attribute;
+}
+
+/*console.log(`\nTarea 6: Obtener attribute`)
+const result5= this.getAttributeFromPredMap(predicateId);
+console.log(`Attribute = ${result5}`);
+*/
+
+/*7. Función para obtener datos de joinCondition */
+exports.getIdsFromJoin=function(joinCondId){
+    var child, parent,elemento6;
+    for(var j in jsonFile["@graph"]){
+        elemento6 = jsonFile["@graph"][j];
+
+        if(elemento6['@id']=== joinCondId){
+            child= elemento6['rr:child'];
+            parent= elemento6['rr:parent']
+        }
+    }
+    return {child,parent};
+}
+
+//console.log(`\nTarea 7: Obtener ids`);
+
+
+
