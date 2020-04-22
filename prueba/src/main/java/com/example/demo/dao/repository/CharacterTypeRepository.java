f0bc7227ab61d520ece9bc3d28e49443b71d4e8c
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:12:48 +0200

    Update CharacterTypeRepository.java

--- prueba/src/main/java/com/example/demo/dao/repository/CharacterTypeRepository.java
+++ prueba/src/main/java/com/example/demo/dao/repository/CharacterTypeRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.dao.repository;
-
+import com.example.demo.dao.entity.*;
+import com.example.demo.dao.entity.Character;
 import java.util.List;
-import com.example.demo.dao.entity.CharacterType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface CharacterTypeRepository extends JpaRepository<CharacterType, String> {
-//List<CharacterType> findCharacterTypeByIdOrName(String id, String name);    
-List<CharacterType> findAllCharacterTypeByIdOrName(String id, String name);  
+List <CharacterType> findAllByIdOrName(String id, String name);
+List <CharacterType> findAllByName(String name);
+/*List <CharacterType> findAllByCharacter(Character character);
+List <CharacterType> findAllByCharacter_CharacterType(CharacterType characterType);
+List <CharacterType> findAllByCharacterAndId(Character character, String id);
+List <CharacterType> findAllByCharacterAndName(Character character, String name);
+*/
 }
