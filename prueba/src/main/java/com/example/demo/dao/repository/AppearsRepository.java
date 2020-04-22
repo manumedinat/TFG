59d7f45628df21dec1b8281e420f03f2e8f7b558
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:12:42 +0200

    Update AppearsRepository.java

--- prueba/src/main/java/com/example/demo/dao/repository/AppearsRepository.java
+++ prueba/src/main/java/com/example/demo/dao/repository/AppearsRepository.java
@@ -1,18 +1,15 @@
 package com.example.demo.dao.repository;
-
-import java.util.List;
-import java.util.Optional;
-
 import com.example.demo.dao.entity.*;
+import com.example.demo.dao.entity.Character;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface AppearsRepository extends JpaRepository<Appears, String> {
-List <Appears> findByCharid(String charid);
-
-//@Query(" where (charid = coalesce(?:charid,charid))")
-List <Appears> findAppearsInByCharid(String charid);
-//List <Episode> findByEpisodes(List<Episode> episodes);
+List <Appears> findAllByCharidOrEpisodeid(String charid, String episodeid);
+List <Appears> findAllByCharid(Character character);
+List <Appears> findAllByCharidAndCharid_id(Character character, String identifier);
+/*List <Appears> findAllByCharacterAndCharid(Character character, String charid);
+List <Appears> findAllByCharacterAndEpisodeid(Character character, String episodeid);
+*/
 }
