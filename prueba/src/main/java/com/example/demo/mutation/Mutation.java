afa032866c6caf6303a800118aeca309321cebba
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:13:00 +0200

    Update Mutation.java

--- prueba/src/main/java/com/example/demo/mutation/Mutation.java
+++ prueba/src/main/java/com/example/demo/mutation/Mutation.java
@@ -1,52 +1,46 @@
 package com.example.demo.mutation;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.example.demo.dao.entity.*;
 import com.example.demo.dao.entity.Character;
 import com.example.demo.service.*;
-
-//import java.time.LocalDate;
 @Component
-public class Mutation implements GraphQLMutationResolver {
-    //APPEARS
-    @Autowired
-    private AppearsService appearsService;
-    public Appears createAppears(String charid, String episodeid) {
-        return this.appearsService.createAppears(charid,episodeid);
-    }
-    //CHARACTER
-    @Autowired
-    private CharacterService characterService;
-    public Character createCharacter(String id, String fname, String lname, String type) {
-        return this.characterService.createCharacter(id,fname,lname,type);
-    }
-    //CHARACTERTYPE
-    @Autowired
-    private CharacterTypeService characterTypeService;
-    public CharacterType createCharacterType(String id, String name) {
-        return this.characterTypeService.createCharacterType(id,name);
-    }
+	public class Mutation implements GraphQLMutationResolver{
+	@Autowired
+	private FriendshipService friendshipService;
+	public Friendship createFriendship(String id,String fid){
+		return this.friendshipService.createFriendship (id,fid);
+	}
 
-    //EPISODE
-    @Autowired
-    private EpisodeService episodeService;
-    public Episode createEpisode(String eid, String ecode) {
-        return this.episodeService.createEpisode(eid, ecode);
-    }
+	@Autowired
+	private HeroesService heroesService;
+	public Heroes createHeroes(String episodeid,String charid){
+		return this.heroesService.createHeroes (episodeid,charid);
+	}
 
-    //FRIENDS
-    @Autowired
-    private FriendsService friendsService;
-    public Friends createFriends(String id, String fid) {
-        return this.friendsService.createFriends(id,fid);
-    }
+	@Autowired
+	private AppearsService appearsService;
+	public Appears createAppears(String charid,String episodeid){
+		return this.appearsService.createAppears (charid,episodeid);
+	}
 
-    //HEROES
-    @Autowired
-    private HeroesService heroesService;
-    public Heroes createHeroes(String episodeid, String charid) {
-        return this.heroesService.createHeroes(episodeid, charid);
-    }
+	@Autowired
+	private CharacterService characterService;
+	public Character createCharacter(String id,String fname,String lname){
+		return this.characterService.createCharacter (id,fname,lname);
+	}
+
+	@Autowired
+	private EpisodeService episodeService;
+	public Episode createEpisode(String id,String code){
+		return this.episodeService.createEpisode (id,code);
+	}
+
+	@Autowired
+	private CharacterTypeService characterTypeService;
+	public CharacterType createCharacterType(String id,String name){
+		return this.characterTypeService.createCharacterType (id,name);
+	}
+
 }
