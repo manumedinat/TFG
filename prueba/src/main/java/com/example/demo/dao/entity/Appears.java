76e97971acb6abacf684ea712d86c671ade41b12
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:12:14 +0200

    Update Appears.java

--- prueba/src/main/java/com/example/demo/dao/entity/Appears.java
+++ prueba/src/main/java/com/example/demo/dao/entity/Appears.java
@@ -1,43 +1,24 @@
 package com.example.demo.dao.entity;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
 import javax.persistence.*;
-
-import com.example.demo.dao.repository.EpisodeRepository;
-
 import java.io.Serializable;
-
-
+import java.util.List;
 @Data
 @EqualsAndHashCode
 @Entity
-@Table(name="appears")
-public class Appears implements Serializable {
+@Table (name="appears_SW")
+@IdClass(AppearsId.class)
+public class Appears implements Serializable{
+private static final long serialVersionUID = 1L;
+	@Id
+	@ManyToOne
+	@JoinColumn(name="charid")
+	private Character charid;
 
-    private static final long serialVersionUID = 1L;
-
-
-    @Id
-    @Column(name="ID",nullable = false)
-    @GeneratedValue
-    private int _id;
-
-    @Column(unique = false)
-    private String charid;
-
-    
-/*   @OneToOne
-    @MapsId
-    @JoinColumn(name="charid", unique = false)
-    private Character character;
-    */
-    @Column(name = "episodeid", nullable = false)
-    private String episodeid;
-
-    /*@OneToOne (mappedBy = "appears", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
-    private Episode episode;
-    */
-
+	@Id
+	@ManyToOne
+	@JoinColumn(name="episodeid")
+	private Episode episodeid;
+	
 }
