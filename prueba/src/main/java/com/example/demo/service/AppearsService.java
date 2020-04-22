55c82a7b85593ad62ce2b84397e949dca785e453
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:13:13 +0200

    Update AppearsService.java

--- prueba/src/main/java/com/example/demo/service/AppearsService.java
+++ prueba/src/main/java/com/example/demo/service/AppearsService.java
@@ -1,8 +1,5 @@
 package com.example.demo.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.example.demo.dao.entity.*;
 import com.example.demo.dao.entity.Character;
 import com.example.demo.dao.repository.*;
@@ -10,38 +7,30 @@ import com.example.demo.dao.repository.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AppearsService {
 
-    private final AppearsRepository appearsRepository ;
-    private final EpisodeRepository episodeRepository ;
-
-    public AppearsService(final AppearsRepository appearsRepository, final EpisodeRepository episodeRepository) {
-        this.appearsRepository = appearsRepository;
-        this.episodeRepository = episodeRepository;
-    }
-
-    @Transactional
-    public Appears createAppears(final String charid, final String episodeid) {
-        final Appears appears = new Appears();
-        appears.setCharid(charid);
-        appears.setEpisodeid(episodeid);
-        return this.appearsRepository.save(appears);
-    }
-
-    @Transactional(readOnly = true)
-    public Iterable<Appears> getAllAppears(final String charid) {
-        if(appearsRepository.findByCharid(charid).isEmpty()){
-            return this.appearsRepository.findAll();
-        }
-        return this.appearsRepository.findByCharid(charid);
-    }
-    
-   /* @Transactional(readOnly = true)
-    public List <Episode> getEpisodesFromAppears(final String eid, final String ecode){
-        return episodeRepository.findEpisodeByeidOrEcode(eid, ecode);
-    }*/
-   
+    private final AppearsRepository appearsRepository;
+public AppearsService (final AppearsRepository appearsRepository){
+this.appearsRepository = appearsRepository;
 }
-
+@Transactional
+public Appears createAppears (final String charid, final String episodeid) {
+ final Appears appears = new Appears();
+         //appears.setCharid(charid);
+         //appears.setEpisodeid(episodeid);
+         return this.appearsRepository.save(appears);
+       }
+        
+    @Transactional(readOnly = true)
+       public Iterable<Appears> getAllAppears(final String identifier, final String episodeid) {
+         if(appearsRepository.findAllByCharidOrEpisodeid(identifier,episodeid).isEmpty()){
+            return this.appearsRepository.findAll();
+           }
+            return this.appearsRepository.findAllByCharidOrEpisodeid(identifier,episodeid);
+        }
+    }
