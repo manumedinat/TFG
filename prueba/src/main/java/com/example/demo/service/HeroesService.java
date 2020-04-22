1b44f52ce27c4915c38fe36c82ff8a65b9a8a6b6
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:13:28 +0200

    Update HeroesService.java

--- prueba/src/main/java/com/example/demo/service/HeroesService.java
+++ prueba/src/main/java/com/example/demo/service/HeroesService.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import com.example.demo.dao.entity.*;
 import com.example.demo.dao.entity.Character;
 import com.example.demo.dao.repository.*;
@@ -9,33 +7,41 @@ import com.example.demo.dao.repository.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @Service
 public class HeroesService {
-
-    private final HeroesRepository heroesRepository ;
-
-    public HeroesService(final HeroesRepository heroesRepository) {
-        this.heroesRepository = heroesRepository ;
-    }
-
-    @Transactional
-    public Heroes createHeroes(String episodeid, String charid) {
-        final Heroes heroes = new Heroes();
-        //final Character character= new Character();
-        heroes.setEpisodeid(episodeid);
-        heroes.setCharid(charid);
+@PersistenceContext
+private EntityManager entityManager;
+private final HeroesRepository heroesRepository;
+public HeroesService (final HeroesRepository heroesRepository){
+this.heroesRepository = heroesRepository;
+}
+@Transactional
+public Heroes createHeroes (String episodeid, String charid) {
+ final Heroes heroes = new Heroes();
+        //heroes.setEpisodeid(episodeid);
+        //heroes.setCharid(charid);
         return this.heroesRepository.save(heroes);
     }
 
     @Transactional(readOnly = true)
-    public List<Heroes> getAllHeroes(String episodeid, String charid) {
-      // if(heroesRepository.findHeroesByEpisodeid(episodeid, charid).isEmpty()){
-        if(heroesRepository.findHeroesByEpisodeidOrCharid(episodeid,charid).isEmpty()){
-        return this.heroesRepository.findAll();
+    public List<Heroes> getAllHeroes(String identifier) {
+        List<Heroes> filter;
+        if(identifier==null){
+            filter= this.heroesRepository.findAll();
+        }else{
+            String template= "http://starwars.mappingpedia.linkeddata.es/heroes/";
+            filter= entityManager.createQuery
+            (" SELECT h FROM Heroes h WHERE '" + template + "' || h.episodeid || '/' || h.charid || '' = '" 
+            + identifier + "'" ).getResultList();     
         }
-        return this.heroesRepository.findByEpisodeidOrCharid(episodeid,charid);
+        return filter;
     }
 }
-
