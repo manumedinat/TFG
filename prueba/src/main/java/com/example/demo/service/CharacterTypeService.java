d49a42a538d3fa7324deba42a237d8a53876e353
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:13:17 +0200

    Update CharacterTypeService.java

--- prueba/src/main/java/com/example/demo/service/CharacterTypeService.java
+++ prueba/src/main/java/com/example/demo/service/CharacterTypeService.java
@@ -1,43 +1,49 @@
 package com.example.demo.service;
 
-import java.util.Optional;
-
-import com.example.demo.dao.entity.CharacterType;
+import com.example.demo.dao.entity.*;
 import com.example.demo.dao.entity.Character;
 import com.example.demo.dao.repository.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Service
 public class CharacterTypeService {
-
-    private final CharacterTypeRepository characterTypeRepository ;
-
-    public CharacterTypeService(final CharacterTypeRepository characterTypeRepository) {
-        this.characterTypeRepository = characterTypeRepository ;
-    }
-
-    @Transactional
-    public CharacterType createCharacterType(final String id, final String name) {
-       // final Character character= new Character();
-        final CharacterType characterType = new CharacterType();
-        characterType.setId(id);
+@PersistenceContext
+private EntityManager entityManager;
+private final CharacterTypeRepository characterTypeRepository;
+public CharacterTypeService (final CharacterTypeRepository characterTypeRepository){
+this.characterTypeRepository = characterTypeRepository;
+}
+@Transactional
+public CharacterType createCharacterType (final String id, final String name) {
+ final CharacterType characterType = new CharacterType();
+        //characterType.setId(id);
         characterType.setName(name);
         return this.characterTypeRepository.save(characterType);
     }
 
-    @Transactional(readOnly = true)
-    public Iterable<CharacterType> getAllCharactersType(final String id, final String name) {
-      if (characterTypeRepository.findAllCharacterTypeByIdOrName(id,name).isEmpty()) {
-        return this.characterTypeRepository.findAll();
-      }
-      return this.characterTypeRepository.findAllCharacterTypeByIdOrName(id,name);
-      
+     @Transactional(readOnly = true)
+       public Iterable<CharacterType> getAllCharacterType(final String identifier, final String name) {
+        List<CharacterType> filter; 
+        if (identifier==null && name==null) {
+            filter=this.characterTypeRepository.findAll();
+        }else if(identifier!=null){
+          String template= "http://starwars.mappingpedia.linkeddata.es/type/";
+            filter=entityManager.createQuery
+            (" SELECT ct FROM CharacterType ct WHERE '" + template + "' || ct.id || '' = '" 
+            + identifier + "'" ).getResultList();  
+        }else{
+            filter= this.characterTypeRepository.findAllByName(name);
+        } 
+          return filter;
     }
-
-   
 }
-
