b374101e9cad5863c79436ef051da67c4377bff7
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:13:08 +0200

    Update CharacterResolver.java

--- prueba/src/main/java/com/example/demo/resolver/CharacterResolver.java
+++ prueba/src/main/java/com/example/demo/resolver/CharacterResolver.java
@@ -1,51 +1,102 @@
 package com.example.demo.resolver;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.example.demo.dao.entity.*;
 import com.example.demo.dao.entity.Character;
-import com.example.demo.dao.entity.CharacterType;
-import com.example.demo.dao.entity.Friends;
-import com.example.demo.dao.entity.Appears;
-import com.example.demo.dao.repository.AppearsRepository;
-
+import com.example.demo.dao.repository.*;
+import com.example.demo.service.CharacterService;
 import org.springframework.stereotype.Component;
-import javax.persistence.*;
-
-import com.example.demo.dao.repository.CharacterRepository;
-import com.example.demo.dao.repository.CharacterTypeRepository;
-import com.example.demo.dao.repository.FriendsRepository;
 
 @Component
-public class CharacterResolver implements GraphQLResolver<Character> {
-    protected EntityManager em;
-    private final AppearsRepository appearsRepository;
-    private final FriendsRepository friendsRepository;
-    private final CharacterTypeRepository characterTypeRepository;
-    private final CharacterRepository characterRepository;
-
-    public CharacterResolver(final EntityManager em,final CharacterRepository characterRepository,
-    final AppearsRepository appearsRepository, final FriendsRepository friendsRepository, final CharacterTypeRepository characterTypeRepository) {
-        this.em=em;
-        this.characterRepository= characterRepository;
-        this.appearsRepository = appearsRepository;
-        this.friendsRepository = friendsRepository;
+public class CharacterResolver implements GraphQLResolver <Character>{
+@PersistenceContext
+private EntityManager entityManager;
+private final AppearsRepository appearsRepository;
+private final FriendshipRepository friendshipRepository;
+private final CharacterTypeRepository characterTypeRepository;
+private final CharacterService characterService;
+public CharacterResolver (final CharacterService characterService,
+final AppearsRepository appearsRepository, final FriendshipRepository friendshipRepository,
+ final CharacterTypeRepository characterTypeRepository){
+this.appearsRepository = appearsRepository;
+this.characterService=characterService;
+        this.friendshipRepository = friendshipRepository;
         this.characterTypeRepository= characterTypeRepository;
     }
-
-    public List<Appears> getAppearsIn(final Character character, final String charid) {
-            /*if(appearsRepository.findAppearsInByCharid(charid).isEmpty()){
-            //return appearsRepository.findAll().stream().filter((appears-> character.getId().equals(charid))).collect(Collectors.toList());
-            return appearsRepository.findAll();
-            }*/
-            return appearsRepository.findAppearsInByCharid(character.getId()); 
-            
-           
+    List<Character> prueba2;
+    boolean flag;
+    public List<Appears> getAppearsIn(final Character character, final String identifier) {
+        List<Appears> join = new ArrayList<Appears>();
+        if(identifier!=null){
+            //flag=true;
+            this.characterService.setFlag(true);
+            String template= "http://starwars.mappingpedia.linkeddata.es/movie/";
+            List<Appears> prueba=entityManager.createQuery(
+                    " SELECT ap FROM Character c LEFT JOIN Appears ap ON c.id=ap.charid WHERE '"
+                            + template + "' || ap.charid || '/' || ap.episodeid || '' = '" + identifier + "'")
+                    .getResultList();
+            prueba2= entityManager.createQuery(
+                " SELECT c FROM Character c LEFT JOIN Appears ap ON c.id=ap.charid WHERE '"
+                        + template + "' || ap.charid || '/' || ap.episodeid || '' = '" + identifier + "'")
+                .getResultList();
+            this.characterService.setFilter(prueba2);
+            for(int i=0;i<prueba.size();i++){
+                if(character.equals(prueba.get(i).getCharid())){
+                    join.add(prueba.get(i));
+                }
+            }
+        }else{
+            this.characterService.setFlag(false);
+            //flag=false;
+            join= appearsRepository.findAllByCharid(character);
         }
-    public List<Friends> getFriendship(final Character character, final String id, final String fid) {
-        return friendsRepository.findAllByIdOrFid(character.getId(), character.getId());
+        return join;
     }
-    public List<CharacterType> getType (final Character character, final String id, final String name){
-        return characterTypeRepository.findAllCharacterTypeByIdOrName(character.getId(),name);
-    }    
-
+    public List<Friendship> getFriends(final Character character, final String identifier, final String id,final String fid) {
+        if(identifier!=null){
+            return friendshipRepository.findAllByIdOrFid(identifier,fid);
+        } else if (fid!=null){
+            return friendshipRepository.findAllByIdOrFid(identifier,fid);
+        }
+        return friendshipRepository.findAllByIdOrFid(identifier,fid);
+    }
+    public List<CharacterType> getType (final Character character, final String identifier, final String name){
+        List<CharacterType> join = new ArrayList<CharacterType>();
+        if(identifier!=null){
+            String template= "http://starwars.mappingpedia.linkeddata.es/type/";
+            List<CharacterType> prueba;
+            prueba=entityManager.createQuery(
+                    " SELECT DISTINCT ct FROM Character c LEFT JOIN CharacterType ct ON c.typeid=ct.id WHERE '"
+                            + template + "' || ct.id || '' = '" + identifier + "'")
+                    .getResultList();
+            for(int i=0;i<prueba.size();i++){
+                if(character.getTypeid().getId().equals(prueba.get(i).getId())){
+                    join.add(prueba.get(i));
+                }
+            }
+            //return characterTypeRepository.findAllByIdOrName(identifier, name);
+        } else if (name!=null){
+            if(name.equals(character.getTypeid().getName())){
+                join.add(character.getTypeid());
+            }
+        }else{
+            join.add(character.getTypeid());
+        }
+        return join;
+        //return characterTypeRepository.findAllByIdOrName(id, name);
+    }
+    /*
+    public boolean getFlag(){
+        return flag;
+    }
+    public List<Character> getList(){
+        return prueba2;
+    }
+    */
 }
