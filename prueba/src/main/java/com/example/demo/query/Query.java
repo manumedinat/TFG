9bf75c963cfb0807c89d85000e1b108a86c85a09
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:13:02 +0200

    Update Query.java

--- prueba/src/main/java/com/example/demo/query/Query.java
+++ prueba/src/main/java/com/example/demo/query/Query.java
@@ -1,49 +1,46 @@
 package com.example.demo.query;
-
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.example.demo.dao.entity.*;
 import com.example.demo.dao.entity.Character;
 import com.example.demo.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 @Component
-public class Query implements GraphQLQueryResolver{
-    //APPEARS
-    @Autowired
-    private AppearsService appearsService;
-    public Iterable<Appears> listAppears(final String charid) {
-        return this.appearsService.getAllAppears(charid);
-    }
-    //CHARACTER
-    @Autowired
-    private CharacterService characterService;
-    public Iterable<Character> listCharacter(String id, String fname, String lname, String type) {
-        return this.characterService.getAllCharacters(id,fname,lname,type);
-    }
+	public class Query implements GraphQLQueryResolver{
+	@Autowired
+	private FriendshipService friendshipService;
+	public Iterable <Friendship> listFriendship(String identifier,String id,String fid){
+		return this.friendshipService.getAllFriendship (identifier, id,fid);
+	}
 
-    //CHARACTERTYPE
-    @Autowired
-    private CharacterTypeService characterTypeService;
-    public Iterable<CharacterType> listCharacterType(String id, String name) {
-        return this.characterTypeService.getAllCharactersType(id,name);
-    }
-    //EPISODE
-    @Autowired
-    private EpisodeService episodeService;
-    public Iterable<Episode> listEpisode(String eid, String ecode) {
-        return this.episodeService.getAllEpisodes(eid, ecode);
-    }
-    //FRIENDS
-    @Autowired
-    private FriendsService friendsService;
-    public Iterable<Friends> listFriends(String id, String fid) {
-        return this.friendsService.getAllFriends(id, fid);
-    }
-    //HEROES
-    @Autowired
-    private HeroesService heroesService;
-    public Iterable<Heroes> listHeroes(String episodeid, String charid) {
-        return this.heroesService.getAllHeroes(episodeid, charid);
-    }
+	@Autowired
+	private HeroesService heroesService;
+	public Iterable <Heroes> listHeroes(String identifier){
+		return this.heroesService.getAllHeroes (identifier);
+	}
+
+	@Autowired
+	private AppearsService appearsService;
+	public Iterable <Appears> listAppears(String identifier,String episodeid){
+		return this.appearsService.getAllAppears (identifier,episodeid);
+	}
+
+	@Autowired
+	private CharacterService characterService;
+	public Iterable <Character> listCharacter(String identifier,String fname,String lname){
+		return this.characterService.getAllCharacter (identifier,fname,lname);
+	}
+
+	@Autowired
+	private EpisodeService episodeService;
+	public Iterable <Episode> listEpisode(String identifier,String code){
+		return this.episodeService.getAllEpisode (identifier,code);
+	}
+
+	@Autowired
+	private CharacterTypeService characterTypeService;
+	public Iterable <CharacterType> listCharacterType(String identifier,String name){
+		return this.characterTypeService.getAllCharacterType (identifier,name);
+	}
+
 }
