76c8543b0fd976d1cfa585c3b7fbc392aabb4955
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:13:20 +0200

    Update EpisodeService.java

--- prueba/src/main/java/com/example/demo/service/EpisodeService.java
+++ prueba/src/main/java/com/example/demo/service/EpisodeService.java
@@ -1,37 +1,49 @@
 package com.example.demo.service;
 
 import com.example.demo.dao.entity.*;
+import com.example.demo.dao.entity.Character;
 import com.example.demo.dao.repository.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Service
 public class EpisodeService {
-
-    private final EpisodeRepository episodeRepository ;
-
-    public EpisodeService(final EpisodeRepository episodeRepository) {
-        this.episodeRepository = episodeRepository ;
-    }
-
-    @Transactional
-    public Episode createEpisode(String eid, String ecode) {
-        final Episode episode = new Episode();
-        episode.setEcode(ecode);
-        episode.setEid(eid);
+@PersistenceContext
+private EntityManager entityManager;    
+private final EpisodeRepository episodeRepository;
+public EpisodeService (final EpisodeRepository episodeRepository){
+this.episodeRepository = episodeRepository;
+}
+@Transactional
+public Episode createEpisode (String id, String code) {
+ final Episode episode = new Episode();
+        episode.setCode(code);
+        //episode.setId(id);
         return this.episodeRepository.save(episode);
     }
 
     @Transactional(readOnly = true)
-    public Iterable<Episode> getAllEpisodes(String eid, String ecode) {
-        if(episodeRepository.findEpisodeByEidOrEcode(eid, ecode).isEmpty()){
-            return this.episodeRepository.findAll();
+    public Iterable<Episode> getAllEpisode(String identifier, String code) {
+        List<Episode> filter;
+        if(identifier== null && code == null){
+            filter=this.episodeRepository.findAll();
+        }else if(identifier!=null){
+            String template= "http://starwars.mappingpedia.linkeddata.es/episode/";
+            filter=entityManager.createQuery
+            (" SELECT e FROM Episode e WHERE '" + template + "' || e.id || '' = '" 
+            + identifier + "'" ).getResultList();  
+        }else{
+            filter= this.episodeRepository.findAllByCode(code);
         }
-        return this.episodeRepository.findEpisodeByEidOrEcode(eid, ecode);
+        return filter;
     }
    
 }
-
