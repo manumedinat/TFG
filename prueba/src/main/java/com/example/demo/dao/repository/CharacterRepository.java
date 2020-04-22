509f85b77e7f79a96403cb42502a7d61f0faf23d
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-04-22 20:12:45 +0200

    Update CharacterRepository.java

--- prueba/src/main/java/com/example/demo/dao/repository/CharacterRepository.java
+++ prueba/src/main/java/com/example/demo/dao/repository/CharacterRepository.java
@@ -1,19 +1,11 @@
 package com.example.demo.dao.repository;
-
-import java.util.List;
-
+import com.example.demo.dao.entity.*;
 import com.example.demo.dao.entity.Character;
-import com.example.demo.dao.entity.Friends;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface CharacterRepository extends JpaRepository<Character, String> {
-List<Character> findCharacterByIdOrFname(String id, String fname);
-List<Character> findCharactersByIdOrFname(String id, String fname);
-List<Character> findAllById(String id);
-//@Query("SELECT h.charid FROM HEROES h.charid LEFT JOIN characters c on h.charid=c.id")    
-//List<Character> findWithAllDetails(String charid);
-
+public interface CharacterRepository extends JpaRepository<Character,String> {
+List <Character> findAllByIdOrFnameOrLname(String id, String fname, String lname);
+List <Character> findAllByFnameOrLname(String fname, String lname);
 }
