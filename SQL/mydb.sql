3d21ad05d65ce91004917fd7e5a89d83a4908153
manumedinat <55244535+manumedinat@users.noreply.github.com>
2020-05-29 12:52:44 +0200

    Update mydb.sql

--- SQL/mydb.sql
+++ SQL/mydb.sql
@@ -26,9 +26,9 @@ CREATE TABLE `appears_sw` (
   `charid` varchar(255) NOT NULL,
   `episodeid` varchar(255) NOT NULL,
   PRIMARY KEY (`charid`,`episodeid`),
-  KEY `FKpnll5ygw587rw7uorhis2ufu3` (`episodeid`),
-  CONSTRAINT `FK9lne3dfaqwu34htdscipljful` FOREIGN KEY (`charid`) REFERENCES `character_sw` (`id`),
-  CONSTRAINT `FKpnll5ygw587rw7uorhis2ufu3` FOREIGN KEY (`episodeid`) REFERENCES `episode_sw` (`id`)
+  KEY `FKfeg9xsyj763n8n0akjc6bqhqu` (`episodeid`),
+  CONSTRAINT `FKatwrkj58fr5n2lgqkrrcjvx04` FOREIGN KEY (`charid`) REFERENCES `characters_sw` (`id`),
+  CONSTRAINT `FKfeg9xsyj763n8n0akjc6bqhqu` FOREIGN KEY (`episodeid`) REFERENCES `episodes_sw` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -38,70 +38,46 @@ CREATE TABLE `appears_sw` (
 
 LOCK TABLES `appears_sw` WRITE;
 /*!40000 ALTER TABLE `appears_sw` DISABLE KEYS */;
-INSERT INTO `appears_sw` VALUES ('1000','4'),('1001','4'),('1002','4'),('2000','4'),('2001','4'),('1000','5'),('1001','5'),('1002','5'),('1003','5'),('2000','5'),('2001','5'),('1000','6'),('1001','6'),('1002','6'),('1003','6'),('2001','6');
+INSERT INTO `appears_sw` VALUES ('1000','4'),('1001','4'),('1002','4'),('1003','4'),('2000','4'),('2001','4'),('1000','5'),('1001','5'),('1002','5'),('1003','5'),('2000','5'),('2001','5'),('1000','6'),('1001','6'),('1002','6'),('1003','6'),('2000','6'),('2001','6');
 /*!40000 ALTER TABLE `appears_sw` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `character_sw`
+-- Table structure for table `characters_sw`
 --
 
-DROP TABLE IF EXISTS `character_sw`;
+DROP TABLE IF EXISTS `characters_sw`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `character_sw` (
+CREATE TABLE `characters_sw` (
   `id` varchar(255) NOT NULL,
   `fname` varchar(255) DEFAULT NULL,
   `lname` varchar(255) DEFAULT NULL,
   `typeid` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKsi0tn1scybgwev1cnv8wowa9q` (`typeid`),
-  CONSTRAINT `FKsi0tn1scybgwev1cnv8wowa9q` FOREIGN KEY (`typeid`) REFERENCES `character_type_sw` (`id`)
+  KEY `FKgmfv1f53p6yptiq0kx2govtgv` (`typeid`),
+  CONSTRAINT `FKgmfv1f53p6yptiq0kx2govtgv` FOREIGN KEY (`typeid`) REFERENCES `types_sw` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `character_sw`
+-- Dumping data for table `characters_sw`
 --
 
-LOCK TABLES `character_sw` WRITE;
-/*!40000 ALTER TABLE `character_sw` DISABLE KEYS */;
-INSERT INTO `character_sw` VALUES ('1000','Luke','Skywalker','H'),('1001','Darth','Vader','H'),('1002','Han','Solo','H'),('1003','Leia','Skywalker','H'),('2000','R2','D2','D'),('2001','C3','PO','D');
-/*!40000 ALTER TABLE `character_sw` ENABLE KEYS */;
+LOCK TABLES `characters_sw` WRITE;
+/*!40000 ALTER TABLE `characters_sw` DISABLE KEYS */;
+INSERT INTO `characters_sw` VALUES ('1000','Luke','Skywalker','H'),('1001','Darth','Vader','H'),('1002','Han','Solo','H'),('1003','Leia','Skywalker','H'),('2000','R2','D2','D'),('2001','C3','PO','D');
+/*!40000 ALTER TABLE `characters_sw` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `character_type_sw`
+-- Table structure for table `episodes_sw`
 --
 
-DROP TABLE IF EXISTS `character_type_sw`;
+DROP TABLE IF EXISTS `episodes_sw`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `character_type_sw` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `character_type_sw`
---
-
-LOCK TABLES `character_type_sw` WRITE;
-/*!40000 ALTER TABLE `character_type_sw` DISABLE KEYS */;
-INSERT INTO `character_type_sw` VALUES ('D','Droid'),('H','Human');
-/*!40000 ALTER TABLE `character_type_sw` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `episode_sw`
---
-
-DROP TABLE IF EXISTS `episode_sw`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `episode_sw` (
+CREATE TABLE `episodes_sw` (
   `id` varchar(255) NOT NULL,
   `code` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -109,40 +85,39 @@ CREATE TABLE `episode_sw` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `episode_sw`
+-- Dumping data for table `episodes_sw`
 --
 
-LOCK TABLES `episode_sw` WRITE;
-/*!40000 ALTER TABLE `episode_sw` DISABLE KEYS */;
-INSERT INTO `episode_sw` VALUES ('4','NewHope'),('5','Empire'),('6','Jedi');
-/*!40000 ALTER TABLE `episode_sw` ENABLE KEYS */;
+LOCK TABLES `episodes_sw` WRITE;
+/*!40000 ALTER TABLE `episodes_sw` DISABLE KEYS */;
+INSERT INTO `episodes_sw` VALUES ('4','NewHope'),('5','Empire'),('6','Jedi');
+/*!40000 ALTER TABLE `episodes_sw` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `friendship_sw`
+-- Table structure for table `friends_sw`
 --
 
-DROP TABLE IF EXISTS `friendship_sw`;
+DROP TABLE IF EXISTS `friends_sw`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `friendship_sw` (
+CREATE TABLE `friends_sw` (
   `fid` varchar(255) NOT NULL,
   `id` varchar(255) NOT NULL,
   PRIMARY KEY (`fid`,`id`),
-  KEY `FK7cglxx1dr47g899ywtxpull59` (`id`),
-  CONSTRAINT `FK7cglxx1dr47g899ywtxpull59` FOREIGN KEY (`id`) REFERENCES `character_sw` (`id`),
-  CONSTRAINT `FKsput227q5l9t61yw7s52m5xbh` FOREIGN KEY (`fid`) REFERENCES `character_sw` (`id`)
+  KEY `FKjyxyukp4oxk6o97v2gmkxyeph` (`id`),
+  CONSTRAINT `FKjyxyukp4oxk6o97v2gmkxyeph` FOREIGN KEY (`id`) REFERENCES `characters_sw` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `friendship_sw`
+-- Dumping data for table `friends_sw`
 --
 
-LOCK TABLES `friendship_sw` WRITE;
-/*!40000 ALTER TABLE `friendship_sw` DISABLE KEYS */;
-INSERT INTO `friendship_sw` VALUES ('1001','1000'),('1002','1000'),('1003','1000'),('1000','1001'),('1000','1002'),('2000','1002'),('1000','1003'),('2000','1003'),('1002','2000'),('1003','2000'),('2001','2000'),('2000','2001');
-/*!40000 ALTER TABLE `friendship_sw` ENABLE KEYS */;
+LOCK TABLES `friends_sw` WRITE;
+/*!40000 ALTER TABLE `friends_sw` DISABLE KEYS */;
+INSERT INTO `friends_sw` VALUES ('1001','1000'),('1002','1000'),('1003','1000'),('2000','1000'),('2001','1000'),('1000','1001'),('1000','1002'),('1003','1002'),('2001','1002'),('1000','1003'),('1002','1003'),('2000','1003'),('2001','1003'),('1000','2000'),('1002','2000'),('1003','2000'),('2001','2000'),('1000','2001'),('1002','2001'),('1003','2001');
+/*!40000 ALTER TABLE `friends_sw` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -156,9 +131,9 @@ CREATE TABLE `heroes_sw` (
   `episodeid` varchar(255) NOT NULL,
   `charid` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`episodeid`),
-  KEY `FK77owm02fdsqvumb8fdnf18h7p` (`charid`),
-  CONSTRAINT `FK77owm02fdsqvumb8fdnf18h7p` FOREIGN KEY (`charid`) REFERENCES `character_sw` (`id`),
-  CONSTRAINT `FKivirqd0lpjt7npy1sseq7bptv` FOREIGN KEY (`episodeid`) REFERENCES `episode_sw` (`id`)
+  KEY `FKr1w6hisc6nrkrn1rqrvc0h5sc` (`charid`),
+  CONSTRAINT `FKp27a7tbtha6mwwmw51gl5ifcw` FOREIGN KEY (`episodeid`) REFERENCES `episodes_sw` (`id`),
+  CONSTRAINT `FKr1w6hisc6nrkrn1rqrvc0h5sc` FOREIGN KEY (`charid`) REFERENCES `characters_sw` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -171,6 +146,30 @@ LOCK TABLES `heroes_sw` WRITE;
 INSERT INTO `heroes_sw` VALUES ('5','1000'),('4','2001'),('6','2001');
 /*!40000 ALTER TABLE `heroes_sw` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `types_sw`
+--
+
+DROP TABLE IF EXISTS `types_sw`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `types_sw` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `types_sw`
+--
+
+LOCK TABLES `types_sw` WRITE;
+/*!40000 ALTER TABLE `types_sw` DISABLE KEYS */;
+INSERT INTO `types_sw` VALUES ('D','Droid'),('H','Human');
+/*!40000 ALTER TABLE `types_sw` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -181,4 +180,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-15 13:48:23
+-- Dump completed on 2020-05-28 21:51:23
